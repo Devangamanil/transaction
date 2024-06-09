80e887aac37ee61a2d0b71104a157011df9fe1a2
Devangamanil <devangamanilkumar72@gmail.com>
2024-06-09 15:38:03 +0530

    transaction commit

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3010;
 
 const app = express();
 app.use(cors());
